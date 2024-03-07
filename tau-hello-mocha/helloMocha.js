574bccca42935350cf08ca7771b887b6b8f5120d
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2024-03-07 08:52:17 +0000

    Retry and time out functions

--- tau-hello-mocha/helloMocha.js
+++ tau-hello-mocha/helloMocha.js
@@ -10,7 +10,17 @@
 const assert = require("assert");
 
 describe("Mathematical Operations - Test Suite", function () {
+  //Time out here is set for the whole test suite
+  // this.timeout(100);
+
   it("Addition of two numbers", function () {
+    // Time out here will be considered for this single test case
+    // this.timeout(100);
+    /* Standard Mocha time out is 2000, so this test case would
+    fail in case we set it for longer than that
+    */
+    // setTimeout(done, 3000);
+
     let a = 10;
     let b = 10;
     let result = a + b;
